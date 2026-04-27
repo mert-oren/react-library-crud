bba2b707bf779e479a600911163cb78dec814634
mert-oren <mertoren92@gmail.com>
2026-04-27 14:37:19 +0200

    Update MainLayout.tsx

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,5 +1,21 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <div>
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default MainLayout;

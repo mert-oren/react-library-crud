ce7ccb62e3350c99414c9da4340526399effa35c
mert-oren <mertoren92@gmail.com>
2026-04-27 14:32:44 +0200

    created Routes

--- src/routes/MyRouter.tsx
+++ src/routes/MyRouter.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
+import AddBook from "../pages/AddBook";
+import Books from "../pages/Books";
+import BookDetail from "../pages/BookDetail";
+import EditBook from "../pages/EditBook";
+import DeleteBook from "../pages/DeleteBook";
+
 const MyRouter = () => {
-  return <div>MyRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/books/:id" element={<BookDetail />} />
+          <Route path="/books/:id/edit" element={<EditBook />} />
+          <Route path="/books/:id/delete" element={<DeleteBook />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default MyRouter;
